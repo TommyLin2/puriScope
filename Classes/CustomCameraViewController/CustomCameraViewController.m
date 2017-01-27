46124a5aa076559e5afab1d194eea041a75b557f
Jordialbers <jordialbers81@gmail.com>
2017-01-27 02:12:13 +0100

    added AVCamCaptureManager

--- Classes/CustomCameraViewController/CustomCameraViewController.m
+++ Classes/CustomCameraViewController/CustomCameraViewController.m
@@ -32,6 +32,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+-(BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+
 /*
 #pragma mark - Navigation
 
